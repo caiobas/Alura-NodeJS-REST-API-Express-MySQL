a21883103904889ab45b888767d6bf0904929aff
caiobas <caiobas6@hotmail.com>
2021-04-29 09:52:52 -0300

    05 - DELETE

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -37,7 +37,7 @@ class Atendimento {
                 if(erro) {
                     res.status(400).json(erro)
                 } else {
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
                 }
             })
         }
@@ -79,7 +79,19 @@ class Atendimento {
             if(erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
